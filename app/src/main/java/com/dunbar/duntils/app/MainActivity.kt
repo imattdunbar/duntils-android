17f542a9e40ca908d8a1e2a076343952ac488f1f
Matt Dunbar <me@mattdunbar.io>
2024-05-08 14:39:56 -0400

    init

---
+++ app/src/main/java/com/dunbar/duntils/app/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.dunbar.duntils.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.dunbar.duntils.TestObject
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Button(onClick = {
+                    TestObject.doesItWork()
+                }) {
+                    Text("Test", color = Color.White)
+                }
+            }
+        }
+    }
+}
---
+++ duntils/src/main/java/com/dunbar/duntils/TestObject.kt
@@ -0,0 +1,9 @@
+package com.dunbar.duntils
+
+import android.util.Log
+
+object TestObject {
+    fun doesItWork() {
+        Log.i("Duntils", "It works!")
+    }
+}
